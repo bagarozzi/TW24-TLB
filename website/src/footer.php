c73e92ec6314315703b16e363b75555f6fa50184
Federico Bagattoni <federico.bagattoni@studio.unibo.it>
2025-01-19 19:50:52 +0100

    Added resources and initial nav bar

---
+++ website/src/footer.php
@@ -0,0 +1,5 @@
+<footer>
+        <p>&copy; 2025 Il tuo sito. Tutti i diritti riservati.</p>
+    </footer>
+</body>
+</html>
---
+++ website/src/index.php
@@ -0,0 +1,6 @@
+<?php include 'header.php'; ?>
+<main>
+    <h2>Home Page</h2>
+    <p>Benvenuto nella home page!</p>
+</main>
+<?php include 'footer.php'; ?>
