0a1de856e7955cb9b30d94224f52389e5e3b256d
Federico Bagattoni <federico.bagattoni@studio.unibo.it>
2025-01-19 21:53:14 +0100

    Added changes and more resources

--- website/src/header.php
+++ website/src/header.php
@@ -11,10 +11,31 @@
     <header>
         <nav class="navbar navbar-light bg-light">
             <div class="container-fluid d-flex justify-content-between">
-                <!--> Hamburger menu <-->
+                <!-- Hamburger menu -->
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
                 </button>
+                <!-- Brand -->
+                <a class="navbar-brand" href="#">
+                <img src="resources/logoat.jpg" alt="" width="60" height="60" class="d-inline-block align-text-center">
+                HarvestHub
+                </a>
+                <!-- Shopping cart -->
+                <div class="d-flex">
+                    <div class="nav-item">
+                        <a class="nav-link" href="#">
+                            <img src="resources/profile.png" alt="" width="30" height="30" class="d-inline-block align-text-top">
+                        </a>
+                    </div>
+                    <!-- Profile -->
+                    <!-- TODO: quando un utente non è loggato deve essere un dropdown -->
+                    <div class="nav-item">
+                        <a class="nav-link" href="#">
+                            <img src="resources/cart.png" alt="" width="30" height="30" class="d-inline-block align-text-top">
+                        </a>
+                    </div>
+                </div>
+                <!-- Expansion -->
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                         <li class="nav-item">
@@ -28,26 +49,14 @@
                         </li>
                     </ul>
                 </div>
-                <!--> Brand <-->
-                <a class="navbar-brand" href="#">
-                <img src="resources/logoat.jpg" alt="" width="30" height="24" class="d-inline-block align-text-top">
-                HarvestHub
-                </a>
-                <div class="container-fluid">
-                    <!--> Shopping cart <-->
-                    <div class="nav-item">
-                        <a class="nav-link" href="#">
-                            <img src="resources/profile.png" alt="" width="30" height="24" class="d-inline-block align-text-top">
-                        </a>
-                    </div>
-                    <!--> Profile <-->
-                    <!--> TODO: quando un utente non è loggato deve essere un dropdown -->
-                    <div class="nav-item">
-                        <a class="nav-link" href="#">
-                            <img src="resources/cart.png" alt="" width="30" height="24" class="d-inline-block align-text-top">
-                        </a>
-                    </div>
-                </div>
             </div>
         </nav>
+        <form class="form-inline">
+            <div class="form-group mx-sm-3 mb-2">
+                <input type="text" class="form-control col-md-2" id="inputPassword2" placeholder="search HarvestHub">
+                <button type="submit" class="btn btn-primary rounded-circle mb-2">
+                    <img src="resources/search.png" alt="" width="20" height="20" class="d-inline-block align-text-top">
+                </button>
+            </div>
+        </form>
     </header>
