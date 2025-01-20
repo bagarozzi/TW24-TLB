1f3268522337193a8135bbdc67e0944bd8dfb20c
Federico Bagattoni <federico.bagattoni@studio.unibo.it>
2025-01-20 22:24:36 +0100

    Added search bar styling

--- website/src/header.php
+++ website/src/header.php
@@ -6,6 +6,11 @@
     <title>Il tuo sito</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+    <style>
+        div:has(> form){
+            z-index: 10;
+        }
+    </style>
 </head>
 <body>
     <header>
@@ -51,12 +56,12 @@
                 </div>
             </div>
         </nav>
-        <form class="form-inline">
-            <div class="form-group mx-sm-3 mb-2">
-                <input type="text" class="form-control col-md-2" id="inputPassword2" placeholder="search HarvestHub">
-                <button type="submit" class="btn btn-primary rounded-circle mb-2">
-                    <img src="resources/search.png" alt="" width="20" height="20" class="d-inline-block align-text-top">
-                </button>
-            </div>
-        </form>
+        <div class="container-fluid position-absolute justify-content-center w-100">
+            <form class="form-inline">
+                <div class="form-group d-flex row justify-content-center align-items-center">
+                    <input type="text" class="form-control w-50 d-inline" id="inputPassword2" placeholder="search the HarvestHub">
+                    <button type="submit" class="btn btn-primary w-25 d-inline col-rounded-circle mb-2"><img src="resources/search.png" alt="" width="20" height="20" class="d-inline-block align-text-top"></button>
+                </div>
+            </form>
+        </div>
     </header>
