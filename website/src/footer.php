7091cfc515d844788b9c7dcc130f824183cceaef
Luca Venturini <luca.venturini14@studio.unibo.it>
2025-01-30 17:17:02 +0100

    implementation of the footer

--- website/src/footer.php
+++ website/src/footer.php
@@ -1,5 +1,34 @@
 <footer>
-        <p>&copy; 2025 Il tuo sito. Tutti i diritti riservati.</p>
-    </footer>
+    <style>
+        footer {
+            background-image: url("resources/footer.png");
+            background-size: cover;
+            background-repeat: no-repeat;
+            height: 150px; /* Aggiunto per rendere visibile il footer */
+            width: 100%;
+            position: relative; /* Permette all'img di posizionarsi dentro */
+            display: flex;
+            align-items: center;
+        }
+ 
+        #button-image {
+            position: absolute;
+            left: 15%;
+            width: auto; /* Regola la dimensione se necessario */
+            height: 30px;
+        }
+
+        p {
+            position: absolute;
+            color: white;
+            text-align: center;
+            font-weight: bold;
+            left: 60%;
+        }
+    </style>
+
+    <img id="button-image" src="resources/buttonList.png" alt="Pulsante Lista">
+    <p>&copy; 2025 Il tuo sito. Tutti i diritti riservati.</p>
+</footer>
 </body>
 </html>
