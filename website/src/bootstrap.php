b9328885d6f0907746e0b834799271146222059c
Jacopo Turchi <jacopo.turchi@studio.unibo.it>
2025-01-31 12:08:57 +0100

    Added bootstrap and database connection

---
+++ website/src/bootstrap.php
@@ -0,0 +1,12 @@
+<?php
+session_start();
+define("RESOURCES_DIR", "./resources/");
+define("SLIDES_DIR", RESOURCES_DIR . "slides/");
+//define("PRODUCTS_DIR", RESOURCES_DIR . "products/");
+//require_once("utils/functions.php");
+require_once("db/database.php");
+$dbh = new DatabaseHelper("localhost", "root", "", "HarvestHub", 3306);
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+?>
