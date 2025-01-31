6573fdf2d9edc04b3c9e3f5320d95f8d76d02368
Federico Bagattoni <federico.bagattoni@studio.unibo.it>
2025-01-31 17:13:33 +0100

    Added better tabs

--- website/src/index.php
+++ website/src/index.php
@@ -15,86 +15,88 @@
 <body>
 <?php include 'header.php'; ?>
 <main>
-<div id="carouselExampleCaptions" class="carousel slide">
-  <div class="carousel-indicators">
-    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
-    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
-    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
-  </div>
-  <div class="carousel-inner">
-    <div class="carousel-item active">
-      <img src="resources/slides/cow.jpg" class="d-block w-100 h-25" alt="...">
-      <div class="carousel-caption d-none d-md-block">
-        <h5>First slide label</h5>
-        <p>Some representative placeholder content for the first slide.</p>
+  <!-- <div class="container"> -->
+    <div id="carouselExampleCaptions" class="carousel slide">
+      <div class="carousel-indicators">
+        <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+        <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
+        <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
       </div>
-    </div>
-    <div class="carousel-item">
-      <img src="resources/slides/fert.jpg" class="d-block w-100 h-25" alt="...">
-      <div class="carousel-caption d-none d-md-block">
-        <h5>Second slide label</h5>
-        <p>Some representative placeholder content for the second slide.</p>
-      </div>
-    </div>
-    <div class="carousel-item">
-      <img src="..." class="d-block w-100" alt="...">
-      <div class="carousel-caption d-none d-md-block">
-        <h5>Third slide label</h5>
-        <p>Some representative placeholder content for the third slide.</p>
-      </div>
-    </div>
-  </div>
-  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
-    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-    <span class="visually-hidden">Previous</span>
-  </button>
-  <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
-    <span class="carousel-control-next-icon" aria-hidden="true"></span>
-    <span class="visually-hidden">Next</span>
-  </button>
-</div>
-    <div class="container w-75">
-        <div class="row">
-            <div class="col-md-6 mb-3 mb-sm-0 p-4">
-                <div class="card text-bg-dark">
-                    <img src="resources/slides/fert.jpg" class="card-img" alt="...">
-                    <div class="card-img-overlay">
-                        <h5 class="card-title">Fertilizers</h5>
-                        <p class="card-text">See our selection of agriculture products</p>
-                    </div>
-                </div>
-            </div>
-            <div class="col-md-6 p-4">
-                <div class="card text-bg-dark">
-                    <img src="resources/slides/cow.jpg" class="card-img" alt="...">
-                    <div class="card-img-overlay">
-                        <h5 class="card-title">Cattle</h5>
-                        <p class="card-text">Browse items for cattle's care</p>
-                    </div>
-                </div>
-            </div>
+      <div class="carousel-inner">
+        <div class="carousel-item active">
+          <img src="resources/slides/cow.jpg" class="d-block w-100" alt="...">
+          <div class="carousel-caption d-none d-md-block">
+            <h1>First slide label</h1>
+            <p>Some representative placeholder content for the first slide.</p>
+          </div>
         </div>
-        <div class="row">
-            <div class="col-md-6 mb-3 mb-sm-0 p-4">
-                <div class="card text-bg-dark">
-                    <img src="resources/slides/fert.jpg" class="card-img" alt="...">
-                    <div class="card-img-overlay">
-                        <h5 class="card-title">Fertilizers</h5>
-                        <p class="card-text">See our selection of agriculture products</p>
-                    </div>
-                </div>
-            </div>
-            <div class="col-md-6 p-4">
-                <div class="card text-bg-dark">
-                    <img src="resources/slides/cow.jpg" class="card-img" alt="...">
-                    <div class="card-img-overlay">
-                        <h5 class="card-title">Cattle</h5>
-                        <p class="card-text">Browse items for cattle's care</p>
-                    </div>
-                </div>
-            </div>
+        <div class="carousel-item">
+          <img src="resources/slides/fert.jpg" class="d-block w-100" alt="...">
+          <div class="carousel-caption d-none d-md-block">
+            <h1>Second slide label</h1>
+            <p>Some representative placeholder content for the second slide.</p>
+          </div>
         </div>
+        <div class="carousel-item">
+          <img src="..." class="d-block w-100" alt="...">
+          <div class="carousel-caption d-none d-md-block">
+            <h1>Third slide label</1>
+            <p>Some representative placeholder content for the third slide.</p>
+          </div>
+        </div>
+      </div>
+      <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
+        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+        <span class="visually-hidden">Previous</span>
+      </button>
+      <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
+        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+        <span class="visually-hidden">Next</span>
+      </button>
     </div>
+  <!-- </div> -->
+  <div class="container w-75">
+      <div class="row">
+          <div class="col-md-6 mb-3 mb-sm-0 p-4">
+              <div class="card text-bg-dark">
+                  <img src="resources/slides/fert.jpg" class="card-img" alt="...">
+                  <div class="card-img-overlay">
+                      <h5 class="card-title">Fertilizers</h5>
+                      <p class="card-text">See our selection of agriculture products</p>
+                  </div>
+              </div>
+          </div>
+          <div class="col-md-6 p-4">
+              <div class="card text-bg-dark">
+                  <img src="resources/slides/cow.jpg" class="card-img" alt="...">
+                  <div class="card-img-overlay">
+                      <h5 class="card-title">Cattle</h5>
+                      <p class="card-text">Browse items for cattle's care</p>
+                  </div>
+              </div>
+          </div>
+      </div>
+      <div class="row">
+          <div class="col-md-6 mb-3 mb-sm-0 p-4">
+              <div class="card text-bg-dark">
+                  <img src="resources/slides/fert.jpg" class="card-img" alt="...">
+                  <div class="card-img-overlay">
+                      <h5 class="card-title">Fertilizers</h5>
+                      <p class="card-text">See our selection of agriculture products</p>
+                  </div>
+              </div>
+          </div>
+          <div class="col-md-6 p-4">
+              <div class="card text-bg-dark">
+                  <img src="resources/slides/cow.jpg" class="card-img" alt="...">
+                  <div class="card-img-overlay">
+                      <h5 class="card-title">Cattle</h5>
+                      <p class="card-text">Browse items for cattle's care</p>
+                  </div>
+              </div>
+          </div>
+      </div>
+  </div>
 </main>
 <?php include 'footer.php'; ?>
 </body>
