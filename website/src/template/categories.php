b21cce2cc3e1e02f29dd69e070248d14bfac35be
Federico Bagattoni <federico.bagattoni@studio.unibo.it>
2025-02-01 15:36:16 +0100

    Redone index

---
+++ website/src/template/categories.php
@@ -0,0 +1,42 @@
+    <div class="container w-75">
+        <div class="row">
+            <div class="col-md-6 mb-3 mb-sm-0 p-4">
+                <div class="card text-bg-dark">
+                    <img src="resources/slides/fert.jpg" class="card-img" alt="...">
+                    <div class="card-img-overlay">
+                        <h5 class="card-title">Fertilizers</h5>
+                        <p class="card-text">See our selection of agriculture products</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-6 p-4">
+                <div class="card text-bg-dark">
+                    <img src="resources/slides/cow.jpg" class="card-img" alt="...">
+                    <div class="card-img-overlay">
+                        <h5 class="card-title">Cattle</h5>
+                        <p class="card-text">Browse items for cattle's care</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-6 mb-3 mb-sm-0 p-4">
+                <div class="card text-bg-dark">
+                    <img src="resources/slides/fert.jpg" class="card-img" alt="...">
+                    <div class="card-img-overlay">
+                        <h5 class="card-title">Fertilizers</h5>
+                        <p class="card-text">See our selection of agriculture products</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-6 p-4">
+                <div class="card text-bg-dark">
+                    <img src="resources/slides/cow.jpg" class="card-img" alt="...">
+                    <div class="card-img-overlay">
+                        <h5 class="card-title">Cattle</h5>
+                        <p class="card-text">Browse items for cattle's care</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
