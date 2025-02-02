1e04ee27c271d79d862e90d483c00e18808210f6
Federico Bagattoni <federico.bagattoni@studio.unibo.it>
2025-02-02 17:16:49 +0100

    Create base template

--- website/src/index.php
+++ website/src/index.php
@@ -1,19 +1,8 @@
 <?php
 require_once 'bootstrap.php';
 $templateParams["nome"] = "mainpage.php";
+$templateParams["styleSheet"] = "css/index.css";
 $templateParams["includeSearchbar"] = true;
-?>
 
-<!DOCTYPE html>
-    <html lang="en">
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Il tuo sito</title>
-        <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons/font/bootstrap-icons.css" rel="stylesheet">
-        <link rel="stylesheet" href="css/index.css">
-    </head>
-    <body>
-        <?php require 'template/base.php';?>
-    </body>    
-</html>
+require 'template/base.php';
+?>
