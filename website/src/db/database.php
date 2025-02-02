a9b97c1a48cec3c8a0b5f382dd4584b04006d1e6
Luca Venturini <luca.venturini14@studio.unibo.it>
2025-02-02 11:42:47 +0100

    added get products

--- website/src/db/database.php
+++ website/src/db/database.php
@@ -8,5 +8,14 @@ class DatabaseHelper {
             die("Connection failed: " . $this->db->connect_error);
         }
     }
+
+    public function getProductsByCategory($category) {
+        $stmt = $this->db->prepare("SELECT * FROM etichetta AS e , prodotto AS p
+                                    WHERE p.codProdotto = e.codProdotto AND e.nome = ?");
+        $stmt->bind_param("s", $category);
+        $stmt->execute();
+        $result = $stmt->get_result();
+        return $result->fetch_all();
+    }
 }
 ?>
