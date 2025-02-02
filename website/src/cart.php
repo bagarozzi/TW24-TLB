dfdb466f993961606cb53ef5fb677e16ad597f2d
Federico Bagattoni <federico.bagattoni@studio.unibo.it>
2025-02-02 16:05:55 +0100

    Made responsive cards

--- website/src/cart.php
+++ website/src/cart.php
@@ -11,50 +11,119 @@ require_once 'bootstrap.php';
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
         <link rel="stylesheet" href="css/cart.css">
+        <script src="js/cart.js"></script>
     </head>
     <body>
         <?php require 'template/header.php';?>
         <main>
+            <div class="container-fluid d-flex justify-content-center">
+                <h1>Shopping cart</h1>
+            </div>
             <div class="row">
-                <div class="col-12 col-lg-8">
-                    <div class="card w-100">
-                        <div class="card-body">
-                            <h5 class="card-title">Trattore</h5>
-                            <h6 class="card-subtitle mb-2 text-muted">Quantità: 2</h6>
-                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                            <a href="#" class="card-link">Card link</a>
-                            <a href="#" class="card-link">Another link</a>
+                <div class="col-12 col-lg-8 px-5">
+                <div class="card w-100 border-0">
+                        <div class="card-body d-flex justify-content-center">
+                            <div class="col d-flex align-items-center justify-content-center">
+                                <h5 class="card-title">Item</h5>
+                            </div>
+                            <div class="col d-flex align-items-center justify-content-center">
+                                <h5 class="card-title">Price</h5>
+                            </div>
+                            <div class="col d-flex align-items-center justify-content-center">
+                                <h5 class="card-title">Quantity</h5>
+                            </div>
                         </div>
                     </div>
-                    <div class="card w-100">
-                        <div class="card-body">
-                            <h5 class="card-title">Trattore</h5>
-                            <h6 class="card-subtitle mb-2 text-muted">Quantità: 2</h6>
-                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                            <a href="#" class="card-link">Card link</a>
-                            <a href="#" class="card-link">Another link</a>
+                    <div class="card w-100 my-2">
+                        <div class="card-body d-flex justify-content-center">
+                            <div class="col d-flex flex-column flex-md-row align-items-center justify-content-center">
+                                <img src="resources/tractor.png" alt="" style="width: 50%; height: auto;">
+                                <div class="d-flex flex-column justify-content-center ms-md-5 mt-3 mt-md-0">
+                                    <h5 class="card-title">John Deere MT-3</h5>
+                                    <h6 class="card-subtitle mb-2 text-muted">Trattore cingolato</h6>
+                                </div>
+                            </div>
+                            <div class="col d-flex align-items-center justify-content-center">
+                            <h5 class="card-title">100.00€</h5>
+                            </div>
+                            <div class="col d-flex flex-column align-items-center justify-content-around">
+                                <div class="d-flex justify-content-between align-items-center">
+                                    <button class="btn btn-primary rounded-circle">-</button>
+                                    <input type="text" class="form-control" value="2">
+                                    <button class="btn btn-primary rounded-circle">+</button>
+                                </div>
+                                <button class="btn btn-danger">Remove item</button>
+                            </div>
                         </div>
                     </div>
-                    <div class="card w-100">
-                        <div class="card-body">
-                            <h5 class="card-title">Trattore</h5>
-                            <h6 class="card-subtitle mb-2 text-muted">Quantità: 2</h6>
-                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                            <a href="#" class="card-link">Card link</a>
-                            <a href="#" class="card-link">Another link</a>
+                    <div class="card w-100 my-2">
+                        <div class="card-body d-flex justify-content-center">
+                            <div class="col d-flex flex-column flex-md-row align-items-center justify-content-center">
+                                <img src="resources/tractor.png" alt="" style="width: 50%; height: auto;">
+                                <div class="d-flex flex-column justify-content-center ms-md-5 mt-3 mt-md-0">
+                                    <h5 class="card-title">John Deere MT-3</h5>
+                                    <h6 class="card-subtitle mb-2 text-muted">Trattore cingolato</h6>
+                                </div>
+                            </div>
+                            <div class="col d-flex align-items-center justify-content-center">
+                            <h5 class="card-title">100.00€</h5>
+                            </div>
+                            <div class="col d-flex flex-column align-items-center justify-content-around">
+                                <div class="d-flex justify-content-between align-items-center">
+                                    <button class="btn btn-primary rounded-circle">-</button>
+                                    <input type="text" class="form-control" value="2">
+                                    <button class="btn btn-primary rounded-circle">+</button>
+                                </div>
+                                <button class="btn btn-danger">Remove item</button>
+                            </div>
                         </div>
                     </div>
-                    <div class="card w-100">
-                        <div class="card-body">
-                            <h5 class="card-title">Trattore</h5>
-                            <h6 class="card-subtitle mb-2 text-muted">Quantità: 2</h6>
-                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                            <a href="#" class="card-link">Card link</a>
-                            <a href="#" class="card-link">Another link</a>
+                    <div class="card w-100 my-2">
+                        <div class="card-body d-flex justify-content-center">
+                            <div class="col d-flex flex-column flex-md-row align-items-center justify-content-center">
+                                <img src="resources/tractor.png" alt="" style="width: 50%; height: auto;">
+                                <div class="d-flex flex-column justify-content-center ms-md-5 mt-3 mt-md-0">
+                                    <h5 class="card-title">John Deere MT-3</h5>
+                                    <h6 class="card-subtitle mb-2 text-muted">Trattore cingolato</h6>
+                                </div>
+                            </div>
+                            <div class="col d-flex align-items-center justify-content-center">
+                            <h5 class="card-title">100.00€</h5>
+                            </div>
+                            <div class="col d-flex flex-column align-items-center justify-content-around">
+                                <div class="d-flex justify-content-between align-items-center">
+                                    <button class="btn btn-primary rounded-circle">-</button>
+                                    <input type="text" class="form-control" value="2">
+                                    <button class="btn btn-primary rounded-circle">+</button>
+                                </div>
+                                <button class="btn btn-danger">Remove item</button>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="card w-100 my-2">
+                        <div class="card-body d-flex justify-content-center">
+                            <div class="col d-flex flex-column flex-md-row align-items-center justify-content-center">
+                                <img src="resources/tractor.png" alt="" style="width: 50%; height: auto;">
+                                <div class="d-flex flex-column justify-content-center ms-md-5 mt-3 mt-md-0">
+                                    <h5 class="card-title">John Deere MT-3</h5>
+                                    <h6 class="card-subtitle mb-2 text-muted">Trattore cingolato</h6>
+                                </div>
+                            </div>
+                            <div class="col d-flex align-items-center justify-content-center">
+                            <h5 class="card-title">100.00€</h5>
+                            </div>
+                            <div class="col d-flex flex-column align-items-center justify-content-around">
+                                <div class="d-flex justify-content-between align-items-center">
+                                    <button class="btn btn-primary rounded-circle">-</button>
+                                    <input type="text" class="form-control" value="2">
+                                    <button class="btn btn-primary rounded-circle">+</button>
+                                </div>
+                                <button class="btn btn-danger">Remove item</button>
+                            </div>
                         </div>
                     </div>
                 </div>
-                <div class="col-12 col-lg-4 d-flex flex-column justify-content-center">
+                <div class="col-12 col-lg-4 d-flex flex-column justify-content-start px-5">
                     <h2>Order summary</h2>
                     <table class="table table-striped">
                         <thead>
@@ -94,7 +163,10 @@ require_once 'bootstrap.php';
                                 <td>3200€</td>
                             </tr>
                         </tfoot>
-                    </table> 
+                    </table>
+                    <div class="d-flex justify-content-center mb-4">
+                        <button class="btn btn-primary">Checkout</button>
+                    </div> 
                 </div>
             </div>
         </main>
