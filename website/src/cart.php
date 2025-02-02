a344917ab4c0cd2028cb4e48bc0cfc291771396c
Federico Bagattoni <federico.bagattoni@studio.unibo.it>
2025-02-02 18:23:24 +0100

    Added db query

--- website/src/cart.php
+++ website/src/cart.php
@@ -4,6 +4,7 @@ $templateParams["nome"] = "shoppingCart.php";
 $templateParams["styleSheet"] = "css/cart.css";
 $templateParams["scriptSheet"] = "js/cart.js";
 $templateParams["includeSearchbar"] = false;
-
+$templateParams["shoppingCart"] = $dbh->getShoppingCart("federicobagattoni@subito.it");
+// AGGIUNGERE IL CONTROLLO DELLA SESSIONE PER IL LOGIN
 require 'template/base.php';
 ?>
--- website/src/db/database.php
+++ website/src/db/database.php
@@ -8,5 +8,14 @@ class DatabaseHelper {
             die("Connection failed: " . $this->db->connect_error);
         }
     }
+
+    public function getShoppingCart($user) {
+        $stmt = $this->db->prepare("SELECT * FROM prodotto, carrello WHERE prodotto.codProdotto=carrello.codProdotto AND carrello.email=?");
+        $stmt->bind_param("s", $user);
+        $stmt->execute();
+        $result = $stmt->get_result();
+
+        return $result->fetch_all(MYSQLI_ASSOC);
+    }
 }
 ?>
