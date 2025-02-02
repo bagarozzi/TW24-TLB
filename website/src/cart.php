2e09802c5f82f4c82bf739cba971d946b6e52ab1
Federico Bagattoni <federico.bagattoni@studio.unibo.it>
2025-02-02 18:56:56 +0100

    Added post request

--- website/src/cart.php
+++ website/src/cart.php
@@ -3,9 +3,31 @@ require_once 'bootstrap.php';
 $templateParams["titolo"] = "Shopping Cart";
 $templateParams["nome"] = "shoppingCart.php";
 $templateParams["styleSheet"] = "css/cart.css";
-$templateParams["scriptSheet"] = "js/cart.js";
+//$templateParams["scriptSheet"] = "js/cart.js";
 $templateParams["includeSearchbar"] = false;
 $templateParams["shoppingCart"] = $dbh->getShoppingCart("federicobagattoni@subito.it");
 // AGGIUNGERE IL CONTROLLO DELLA SESSIONE PER IL LOGIN
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST' && $_POST['action'] === 'update_quantity') {
+    $product_id = $_POST['product_id'];
+    $quantity = (int)$_POST['quantity'];
+
+    if (isset($_POST['increase'])) {
+        $quantity++;
+    } elseif (isset($_POST['decrease'])) {
+        $quantity--;
+    }
+    
+    if($quantity <= 0) {
+        $dbh->removeFromShoppingCart("federicobagattoni@subito.it", $product_id);
+    }
+    else {
+        $dbh->updateShoppingCart("federicobagattoni@subito.it", $product_id, $quantity);
+    }
+
+    header('Location: cart.php');
+    exit;
+}
+
 require 'template/base.php';
 ?>
