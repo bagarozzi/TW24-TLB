a823c092de31b58c9b2099d506ef0330c6362c68
Federico Bagattoni <federico.bagattoni@studio.unibo.it>
2025-02-03 10:19:00 +0100

    Added login requirement

--- website/src/cart.php
+++ website/src/cart.php
@@ -5,8 +5,14 @@ $templateParams["nome"] = "shoppingCart.php";
 $templateParams["styleSheet"] = "css/cart.css";
 //$templateParams["scriptSheet"] = "js/cart.js";
 $templateParams["includeSearchbar"] = false;
-$templateParams["shoppingCart"] = $dbh->getShoppingCart("federicobagattoni@subito.it");
-// AGGIUNGERE IL CONTROLLO DELLA SESSIONE PER IL LOGIN
+
+//if (!isset($_SESSION['username'])) {
+//    header('Location: login.php');
+//    exit;
+//}
+//else {
+    $templateParams["shoppingCart"] = $dbh->getShoppingCart($_SESSION['username']);
+//}
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST' && $_POST['action'] === 'update_quantity') {
     $product_id = $_POST['product_id'];
@@ -19,10 +25,10 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST' && $_POST['action'] === 'update_quanti
     }
     
     if($quantity <= 0) {
-        $dbh->removeFromShoppingCart("federicobagattoni@subito.it", $product_id);
+        $dbh->removeFromShoppingCart($_SESSION['username'], $product_id);
     }
     else {
-        $dbh->updateShoppingCart("federicobagattoni@subito.it", $product_id, $quantity);
+        $dbh->updateShoppingCart($_SESSION['username'], $product_id, $quantity);
     }
 
     header('Location: cart.php');
