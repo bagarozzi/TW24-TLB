ff88e39566f5f054ccb73b2be951cb93ceeebb25
Jacopo Turchi <jacopo.turchi@studio.unibo.it>
2025-02-03 14:17:56 +0100

    implemented

---
+++ website/src/login.php
@@ -0,0 +1,9 @@
+<?php
+require_once 'bootstrap.php';
+$templateParams["titolo"] = "Login";
+$templateParams["nome"] = "login-form.php";
+//$templateParams["styleSheet"] = "css/.css";
+$templateParams["includeSearchbar"] = false;
+
+require 'template/base.php';
+?>
