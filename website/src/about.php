a4bb5e42ac797e1275d067d5bf85782e79155752
Federico Bagattoni <federico.bagattoni@studio.unibo.it>
2025-02-03 16:26:57 +0100

    Added about us page

---
+++ website/src/about.php
@@ -0,0 +1,9 @@
+<?php
+require_once 'bootstrap.php';
+$templateParams["titolo"] = "About the Harvest Hub";
+$templateParams["nome"] = "aboutus.php";
+//$templateParams["styleSheet"] = "css/.css";
+$templateParams["includeSearchbar"] = false;
+
+require 'template/base.php';
+?>
