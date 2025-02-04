afd388e10505ddf6402342a427935895af6e5be7
Luca Venturini <luca.venturini14@studio.unibo.it>
2025-02-04 16:35:42 +0100

    added button for filter

--- website/src/template/scrolling-products.php
+++ website/src/template/scrolling-products.php
@@ -1,5 +1,6 @@
 <div class="container d-flex justify-item-start flex-column pt-5 px-0">
-<div class="dropdown row col-5 col-md-2 pt-2 ps-5">
+<div class="container d-flex flex-wrap">
+<div class="dropdown col-4 col-md-2 pt-2 ps-5">
   <button class="btn dropdown-toggle rounded-pill border" type="button" data-bs-toggle="dropdown" aria-expanded="false">
     Ordina per
   </button>
@@ -7,6 +8,16 @@
     <li><button id="order-by-name" class="dropdown-item" data-sort="nome ASC" data-category="<?php echo $_GET["category"]?>">Nome</button></li>
     <li><button id="order-by-price" class="dropdown-item" data-sort="prezzo ASC" data-category="<?php echo $_GET["category"]?>">Prezzo</button></li>
   </ul>
+</div>
+<div class="dropdown col-4 col-md-2 pt-2 ps-0">
+  <button class="btn dropdown-toggle rounded-pill border" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+    Filtra
+  </button>
+  <ul id= "order-dropdown" class="dropdown-menu">
+    <li><button id="order-by-name" class="dropdown-item" data-sort="nome ASC" data-category="<?php echo $_GET["category"]?>">Nome</button></li>
+    <li><button id="order-by-price" class="dropdown-item" data-sort="prezzo ASC" data-category="<?php echo $_GET["category"]?>">Prezzo</button></li>
+  </ul>
+</div>
 </div>
 <div id="productsContainer" class = "row container pt-3 mb-4 d-flex flex-wrap justify-content-center align-items-center px-0">
    	<?php 
