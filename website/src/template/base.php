92d22e580ee03e59796ba96643a34a917faaedfa
Jacopo Turchi <jacopo.turchi@studio.unibo.it>
2025-02-04 19:41:12 +0100

    moved js

--- website/src/template/base.php
+++ website/src/template/base.php
@@ -13,11 +13,6 @@
     ?>
     <link rel="stylesheet" href="./css/account.css">
     <link rel="stylesheet" href="./css/base.css">
-    <?php
-        if(isset($templateParams["scriptSheet"])) {
-            echo '<script src="' . $templateParams["scriptSheet"] . '"></script>';
-        }
-    ?>
     <script src="js/typewriter.js"></script>
     <link rel="icon" type="image/x-icon" href="resources/favicon.ico">
 </head>
@@ -33,6 +28,11 @@
             ?>
         </main>
         <?php require 'footer.php'; ?>
+        <?php
+            if(isset($templateParams["scriptSheet"])) {
+                echo '<script src="' . $templateParams["scriptSheet"] . '"></script>';
+            }
+        ?>
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
     </body>
 </html>
