d87c3115eab26bcd1e86035dfb44be892a05e0c0
Jacopo Turchi <jacopo.turchi@studio.unibo.it>
2025-02-05 16:39:57 +0100

    Fixed prodouct

--- website/src/single-product.php
+++ website/src/single-product.php
@@ -1,21 +1,24 @@
 <?php
-    require_once 'bootstrap.php';
-    $templateParams["includeSearchbar"] = true;
-    $templateParams["styleSheet"] = "css/product.css";
-    $templateParams["scriptSheet"]  = "js/product.js";
-    $templateParams["nome"] = 'template/show-single-product.php';
-    $templateParams["product"] = $dbh->getSingleProduct($_GET["id"]);
-    $templateParams["titolo"] = $templateParams["product"]["nome"];
-    require 'template/base.php';
+require_once 'bootstrap.php';
 
-
-    if(isset($_POST["action"])) {
-        if(isset($_SESSION["email"])) {
-            $dbh->insertProductInCart($_GET["id"], $_SESSION["email"], $_POST["quantity"]);
-        } else {
-            header("Location: login.php");
-            exit();
-        }
+if(isset($_POST["action"])) {
+    if(isset($_SESSION["email"])) {
+        $dbh->insertProductInCart($_GET["id"], $_SESSION["email"], $_POST["quantity"]);
+        header("Location: " . $_SESSION["previousPage"]);
+        exit();
+    } else {
+        header("Location: login.php");
+        exit();
     }
+}
+
+$templateParams["includeSearchbar"] = true;
+$templateParams["styleSheet"] = "css/product.css";
+$templateParams["scriptSheet"]  = "js/product.js";
+$templateParams["nome"] = 'template/show-single-product.php';
+$templateParams["product"] = $dbh->getSingleProduct($_GET["id"]);
+$templateParams["titolo"] = $templateParams["product"]["nome"];
+require 'template/base.php';
+    
     
 ?>
--- website/src/template/show-single-product.php
+++ website/src/template/show-single-product.php
@@ -5,7 +5,7 @@
             <h3 class="card-title"><?php echo $templateParams["product"]["nome"] ?></h3>
             <h4 class="text-success"><?php echo $templateParams["product"]["prezzo"] ?>€</h4>
             <p class="card-text"><?php echo $templateParams["product"]["descrizione"] ?></p> <!-- Descrizione del prodotto -->
-            <form action="<?php echo (isUserLoggedIn()) ? $_SERVER["PHP_SELF"] . '?' . $_SERVER['QUERY_STRING'] : "./login.php"?>" method="post">
+            <form action="#" method="post">
                 <div class="mb-3">
                     <label for="quantity" class="form-label">Quantità</label>
                     <input type="number" class="form-control" id="quantity" name="quantity" min="1" max="<?php echo $templateParams["product"]["disponibilita"]; ?>" value="1" required>
