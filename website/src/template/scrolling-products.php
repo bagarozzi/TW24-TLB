cf7f4d8a57104f3464c21bb8bbc531c2e6e73c2f
Luca Venturini <luca.venturini14@studio.unibo.it>
2025-02-05 10:50:34 +0100

    added dropdown for filter

--- website/src/template/scrolling-products.php
+++ website/src/template/scrolling-products.php
@@ -1,29 +1,51 @@
-<div class="container d-flex justify-item-start flex-column pt-5 px-0">
-<div class="container d-flex flex-wrap">
-<div class="dropdown col-4 col-md-2 pt-2 ps-5">
-  <button class="btn dropdown-toggle rounded-pill border" type="button" data-bs-toggle="dropdown" aria-expanded="false">
-    Ordina per
-  </button>
-  <ul id= "order-dropdown" class="dropdown-menu">
-    <li><button id="order-by-name" class="dropdown-item" data-sort="nome ASC" data-category="<?php echo $_GET["category"]?>">Nome</button></li>
-    <li><button id="order-by-price" class="dropdown-item" data-sort="prezzo ASC" data-category="<?php echo $_GET["category"]?>">Prezzo</button></li>
-  </ul>
-</div>
-<div class="dropdown col-4 col-md-2 pt-2 ps-0">
-  <button class="btn dropdown-toggle rounded-pill border" type="button" data-bs-toggle="dropdown" aria-expanded="false">
-    Filtra
-  </button>
-  <ul id= "order-dropdown" class="dropdown-menu">
-    <li><button id="order-by-name" class="dropdown-item" data-sort="nome ASC" data-category="<?php echo $_GET["category"]?>">Nome</button></li>
-    <li><button id="order-by-price" class="dropdown-item" data-sort="prezzo ASC" data-category="<?php echo $_GET["category"]?>">Prezzo</button></li>
-  </ul>
-</div>
-</div>
-<div id="productsContainer" class = "row container pt-3 mb-4 d-flex flex-wrap justify-content-center align-items-center px-0">
-   	<?php 
+<div class="container pt-5 px-0">
+	<div class="row mb-3 pt-2">
+		<div id="filter-container" class="col-md-auto d-flex gap-2 ps-md-5 ps-5">
+			<div class="dropdown">
+				<button class="btn dropdown-toggle rounded-pill border" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+					Ordina per
+				</button>
+				<ul id="order-dropdown" class="dropdown-menu">
+					<li><button id="order-by-name" class="dropdown-item" data-sort="nome ASC" data-category="<?php echo $_GET['category']; ?>">Nome</button></li>
+					<li><button id="order-by-price" class="dropdown-item" data-sort="prezzo ASC" data-category="<?php echo $_GET['category']; ?>">Prezzo</button></li>
+				</ul>
+			</div>
+			<div class="dropdown">
+				<button class="btn dropdown-toggle rounded-pill border" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+					Filtri
+				</button>
+				<div class="dropdown-menu" style="min-width:200px;">
+					<div id="dropdown-menu-filter" class="container d-flex flex-column">
+						<label for="categoryFilter" class="form-label">Categoria</label>
+						<select id="categoryFilter" class="form-select">
+							<?php
+							foreach ($templateParams["categories"] as $category) :
+								echo '<option value=' . $category["name"] . '>' . $category["name"] . '</option>';
+							endforeach;
+							?>
+						</select>
+						<label for="priceInput" class="form-label mt-3">
+							Prezzo massimo:
+							<div class="input-group">
+								<input type="number" id="maxPrice" class="form-control" min="0" step="10" value="70000">
+								<span class="input-group-text">€</span>
+							</div>
+							<input type="range" id="priceSlider" class="form-range" min="0" max="70000" step="50">
+						</label>
+						<button id="applyFilters" class="btn btn-primary w-100 mt-3">Applica</button>
+					</div>
+				</div>
+			</div>
+
+		</div>
+	</div>
+
+	<div id="productsContainer" class="row d-flex flex-wrap justify-content-start align-items-stretch px-0">
+		<?php
 		require "display-products.php";
-   		echo display($templateParams["products"]);
-	?>
+		echo display($templateParams["products"]);
+		?>
+	</div>
 </div>
-</div>
-<script src = "js/products.js"></script>
+
+<script src="js/products.js"></script>
