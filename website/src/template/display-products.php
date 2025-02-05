f0c832ada1935c14de9d713573eda380e84fae8d
Luca Venturini <luca.venturini14@studio.unibo.it>
2025-02-05 17:33:21 +0100

    fixed style

--- website/src/template/display-products.php
+++ website/src/template/display-products.php
@@ -2,17 +2,17 @@
 <?php
 function display($products)
 {
-    $html = ""; 
+    $html = "";
     foreach ($products as $product) :
-        $html = '<div class="col-12 col-md-6">
-                    <a type="button" href="single-product.php?id=' . $product["codProdotto"] . '" class="col-10 pt-2 card normal-link mx-auto">
-                        <img src="' . $product["immagine"] . '" class="card-img-top" alt="">
-                        <div class="card-body text-start ps-3">
-                            <h5 class="card-title">' . $product["nome"] . '</h5>
-                            <p class="card-body">' . $product["prezzo"] . '</p>
-                        </div>
-                    </a>
-                  </div>';
+        $html = '<div class="col-12 col-md-6 mb-4 d-flex justify-content-center">
+                <a type="button" href="single-product.php?id=' . $product["codProdotto"] . '" class="card normal-link d-flex flex-column h-100" style=" width: 80%">
+                    <img src="' . $product["immagine"] . '" class="card-img-top mx-auto" alt="' . $product["nome"] . '" style="object-fit: cover; width: 80%; height: 200px;">
+                    <div class="card-body text-start d-flex flex-column" style="flex-grow: 1;">
+                        <h5 class="card-title">' . $product["nome"] . '</h5>
+                        <p class="card-text">' . $product["prezzo"] . '€</p>
+                    </div>
+                </a>
+              </div>';
         echo $html;
     endforeach;
     $_SESSION["previousPage"] = "./products.php?" . http_build_query($_GET);
