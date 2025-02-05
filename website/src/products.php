3f87407cd48b6355991b30002ffb245ca0da3bcd
Luca Venturini <luca.venturini14@studio.unibo.it>
2025-02-05 10:49:44 +0100

    added array categories

--- website/src/products.php
+++ website/src/products.php
@@ -2,14 +2,14 @@
     
     $category=(isset($_GET["category"])) ? $_GET["category"] : ""; 
     $sort=(isset($_GET["sort"])) ? $_GET["sort"] : ""; 
-    $name=(isset($_GET["name"])) ? $_GET["name"] : ""; 
+    $name=(isset($_GET["name"])) ? $_GET["name"] : "";
+    $maxPrice=(isset($_GET["maxPrice"])) ? $_GET["maxPrice"] : ""; 
     
     require_once 'bootstrap.php';
     $templateParams["includeSearchbar"] = true;
     $templateParams["styleSheet"] = "css\listProducts.css";
     $templateParams["nome"] = 'template/scrolling-products.php';
-    if(isset($category)) {
-        $templateParams["products"] = $dbh->getProducts($category, $name, $sort);
-    }
+    $templateParams["categories"] = $dbh->getCategories();
+    $templateParams["products"] = $dbh->getProducts($category, $name, $sort, $maxPrice);
     require 'template/base.php';
 ?>
