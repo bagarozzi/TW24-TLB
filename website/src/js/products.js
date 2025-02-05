ff19f9701c4de6da3d41f68f95908c0d1fdf0159
Luca Venturini <luca.venturini14@studio.unibo.it>
2025-02-05 10:51:56 +0100

    added generic function for filter products and event for filter buttons

--- website/src/js/products.js
+++ website/src/js/products.js
@@ -17,9 +17,49 @@ document.addEventListener('DOMContentLoaded', function () {
                 elemento.remove();
             }
         }
-        sortProducts(sortType, category);
+        sortProducts(sortType);
     });
+
+    const dropdownMenu = document.getElementById('dropdown-menu-filter');
+    // Funzione per evitare che il dropdown si chiuda quando clicchi all'interno del dropdown, ma non su un elemento specifico
+    dropdownMenu.addEventListener("click", function (e) {
+        e.stopPropagation();
+    });
+
+    // Funzione per aggiornare il valore dello slider
+    const priceSlider = document.getElementById("priceSlider");
+    const maxPrice = document.getElementById("maxPrice");
+
+    // Sincronizzare il valore tra lo slider e l'input numerico
+    priceSlider.addEventListener("input", function () {
+        maxPrice.value = priceSlider.value; // Imposta il valore dell'input numerico uguale al valore dello slider
+    });;
+
+    // Funzione per applicare i filtri
+    document.getElementById("applyFilters").addEventListener("click", function () {
+        if (!document.getElementById("clearFilters")) {
+            let nuovoElemento = document.createElement("li"); // Crea un <li>
+            nuovoElemento.innerHTML = '<button id="clearFilters" class="btn btn-danger">❌ Rimuovi filtri</button>'; // Aggiunge testo
+            document.getElementById("filter-container").appendChild(nuovoElemento);
+            nuovoElemento.addEventListener("click", function (e) {
+                updateProducts(["maxPrice", "category", "name"], []);
+                document.getElementById("filter-container").removeChild(nuovoElemento);
+            });
+        }
+        let category = document.getElementById("categoryFilter").value;
+        let price = parseInt(maxPrice.value);
+        filterProducts(price, category);
+    });
+
 })
+
+function filterProducts(price, category) {
+    updateProducts(["maxPrice", "category"], [
+        { name: "maxPrice", value: price },
+        { name: "category", value: category }
+    ]);
+
+}
 
 function changeOrder(element) {
     let [nome, ordinamento] = element.getAttribute("data-sort").split(" ");
@@ -27,11 +67,24 @@ function changeOrder(element) {
     element.setAttribute("data-sort", `${nome} ${ordinamento}`);
 }
 
-function sortProducts(sortType, category) {
-    fetch(`template/products-filtered.php?category=${category}&sort=${sortType}`)
-        .then(response => response.text()) // Ricevi la risposta come testo HTML
+function sortProducts(sortType) {
+    updateProducts(["sort"], [{ name: "sort", value: sortType },]);
+
+}
+
+function updateProducts(paramDelete, newParam) {
+    const params = new URLSearchParams(window.location.search);
+    paramDelete.forEach(param => {
+        params.delete(param);
+    });
+    newParam.forEach(param => {
+        params.append(param["name"], param["value"]);
+    })
+    fetch(`template/products-filtered.php?${params.toString()}`)
+        .then(response => response.text())
         .then(data => {
             document.getElementById("productsContainer").innerHTML = data;
         })
         .catch(error => console.error('Errore:', error));
+    window.history.pushState({}, "", `${window.location.pathname}?${params.toString()}`);
 }
