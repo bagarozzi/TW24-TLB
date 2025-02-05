a466d87e41a06c855c2bd48a34730bd8bd517231
Luca Venturini <luca.venturini14@studio.unibo.it>
2025-02-05 15:06:14 +0100

    moved css to a css file

--- website/src/template/footer.php
+++ website/src/template/footer.php
@@ -1,33 +1,4 @@
-<style>
-	.footer {
-    margin-top: auto; /* Spinge il footer verso il basso */
-}
-
-#footer-container {
-    background-image: url("resources/footer.png");
-    background-size: cover;
-    background-repeat: no-repeat;
-    height: 150px; /* Altezza del footer */
-    width: 100%;
-    position: relative; /* Permette all'immagine di posizionarsi correttamente */
-}
-
-#footer-background-opacity {
-    background-color: rgba(88, 55, 46, 0.8);
-    width: 100%;
-    height: 100%;
-    align-items: center;
-    display: flex;
-    justify-content: center; /* Centra i contenuti orizzontalmente */
-}
-
-.social-button {
-    background: transparent;
-    border: 2px solid transparent;
-    width: auto;
-    height: auto;
-}
-</style>
+<link rel="stylesheet" href="./css/footerStyle.css">
 <footer class="mt-3">
     <div id="footer-container">
         <div id="footer-background-opacity">
