051a5f974bb6717184dce266bcbf07927c01e3a0
Luca Venturini <luca.venturini14@studio.unibo.it>
2025-02-05 13:00:15 +0100

    added query for single product

--- website/src/db/database.php
+++ website/src/db/database.php
@@ -1,51 +1,65 @@
 <?php
-class DatabaseHelper {
+class DatabaseHelper
+{
     private $db;
 
-    public function __construct($servername, $username, $password, $dbname, $port) {
+    public function __construct($servername, $username, $password, $dbname, $port)
+    {
         $this->db = new mysqli($servername, $username, $password, $dbname, $port);
         if ($this->db->connect_error) {
             die("Connection failed: " . $this->db->connect_error);
         }
     }
 
-    public function getProducts($category, $name, $sort, $maxPrice) {
-        $sql = "SELECT nome, prezzo, descrizione, immagine, disponibilita FROM prodotto WHERE 1";
+    public function getSingleProduct($id)
+    {
+        $sql = "SELECT nome, prezzo, descrizione, immagine, disponibilita FROM prodotto WHERE codProdotto = ?";
+        $stmt = $this->db->prepare($sql);
+        $stmt->bind_param("i", $id);
+        $stmt->execute();
+        $result = $stmt->get_result();
+        return $result->fetch_assoc();
+    }
+
+    public function getProducts($category, $name, $sort, $maxPrice)
+    {
+        $sql = "SELECT codProdotto, nome, prezzo, descrizione, immagine, disponibilita FROM prodotto WHERE 1";
         $types = "";
         $params = [];
         $paramYN = false;
-        if($category != "") {
+        if ($category != "") {
             $sql .= " AND prodotto.App_nome = ?";
             $types .= "s";
             $params[] = $category;
             $paramYN = true;
         }
-        if($maxPrice != "") {
+        if ($maxPrice != "") {
             $sql .= " AND prodotto.prezzo < ?";
             $types .= "d";
             $params[] = $maxPrice;
             $paramYN = true;
         }
-        if($name != "") {
+        if ($name != "") {
             $sql .= ' AND LOWER(nome) LIKE ?';
             $types .= "s";
             $name = strtolower($name);
             $params[] = "%$name%";
             $paramYN = true;
         }
-        if($sort !="") {
+        if ($sort != "") {
             $sql .= " ORDER BY $sort";
         }
         $stmt = $this->db->prepare($sql);
-        if($paramYN) {
-            $stmt->bind_param($types, ...$params); // Binding dei parametri
+        if ($paramYN) {
+            $stmt->bind_param($types, ...$params);
         }
         $stmt->execute();
         $result = $stmt->get_result();
         return $result->fetch_all(MYSQLI_ASSOC);
     }
 
-    public function getShoppingCart($user) {
+    public function getShoppingCart($user)
+    {
         $stmt = $this->db->prepare("SELECT * FROM prodotto, carrello WHERE prodotto.codProdotto=carrello.codProdotto AND carrello.email=?");
         $stmt->bind_param("s", $user);
         $stmt->execute();
@@ -54,25 +68,29 @@ class DatabaseHelper {
         return $result->fetch_all(MYSQLI_ASSOC);
     }
 
-    public function emptyCart($email) {
+    public function emptyCart($email)
+    {
         $stmt = $this->db->prepare("DELETE FROM carrello WHERE email=?");
         $stmt->bind_param("s", $email);
         $stmt->execute();
     }
 
-    public function updateShoppingCart($user, $product, $quantity) {
+    public function updateShoppingCart($user, $product, $quantity)
+    {
         $stmt = $this->db->prepare("UPDATE carrello SET quantita=? WHERE email=? AND codProdotto=?");
         $stmt->bind_param("isi", $quantity, $user, $product);
         $stmt->execute();
     }
 
-    public function removeFromShoppingCart($user, $product) {
+    public function removeFromShoppingCart($user, $product)
+    {
         $stmt = $this->db->prepare("DELETE FROM carrello WHERE email=? AND codProdotto=?");
         $stmt->bind_param("si", $user, $product);
         $stmt->execute();
     }
 
-    public function getNotifications($user) {
+    public function getNotifications($user)
+    {
         $stmt = $this->db->prepare("SELECT * FROM notifica WHERE email=? ORDER BY Data DESC");
         $stmt->bind_param("s", $user);
         $stmt->execute();
@@ -80,14 +98,16 @@ class DatabaseHelper {
         return $result->fetch_all(MYSQLI_ASSOC);
     }
 
-    public function getAdminNotifications() {
+    public function getAdminNotifications()
+    {
         $stmt = $this->db->prepare('SELECT * FROM notifica WHERE username="turbo" ORDER BY Data DESC');
         $stmt->execute();
         $result = $stmt->get_result();
         return $result->fetch_all(MYSQLI_ASSOC);
     }
 
-    public function getUserInfo($email) {
+    public function getUserInfo($email)
+    {
         $query = "SELECT email, password, nome as name, cognome as surname, dataNascita as birthday FROM UTENTE WHERE email = ?";
         $stmt = $this->db->prepare($query);
         $stmt->bind_param('s', $email);
@@ -96,21 +116,24 @@ class DatabaseHelper {
         return $result->fetch_all(MYSQLI_ASSOC);
     }
 
-    public function insertUser($email, $password, $name, $surname, $birthday) {
+    public function insertUser($email, $password, $name, $surname, $birthday)
+    {
         $query = "INSERT INTO utente (email, password, nome, cognome, dataNascita) VALUES (?, ?, ?, ?, ?)";
         $stmt = $this->db->prepare($query);
         $stmt->bind_param('sssss', $email, $password, $name, $surname, $birthday);
         $stmt->execute();
     }
 
-    public function updateUser($email, $name, $surname, $birthday, $password) {
+    public function updateUser($email, $name, $surname, $birthday, $password)
+    {
         $query = "UPDATE UTENTE SET name = ?, surname = ?, birthday = ?, password = ? WHERE email = ?";
         $stmt = $this->db->prepare($query);
         $stmt->bind_param('sssss', $name, $surname, $birthday, $password, $email);
         return $stmt->execute();
     }
 
-    public function checkUsername($email) {
+    public function checkUsername($email)
+    {
         $query = "SELECT email FROM UTENTE WHERE email = ?";
         $stmt = $this->db->prepare($query);
         $stmt->bind_param('s', $email);
@@ -119,28 +142,32 @@ class DatabaseHelper {
         return $result->fetch_all(MYSQLI_ASSOC);
     }
 
-    public function insertCategory($category) {
+    public function insertCategory($category)
+    {
         $query = "INSERT INTO CATEGORIA_PRODOTTO (nome) VALUES (?)";
         $stmt = $this->db->prepare($query);
         $stmt->bind_param("s", $category);
         $stmt->execute();
     }
 
-    public function updateCategory($newCategory, $category) {
+    public function updateCategory($newCategory, $category)
+    {
         $query = "UPDATE CATEGORIA_PRODOTTO SET nome = ? WHERE nome = ?";
         $stmt = $this->db->prepare($query);
         $stmt->bind_param("ss", $newCategory, $category);
         return $stmt->execute();
     }
 
-    public function deleteCategory($category) {
+    public function deleteCategory($category)
+    {
         $query = "DELETE FROM CATEGORIA_PRODOTTO WHERE nome = ?";
         $stmt = $this->db->prepare($query);
         $stmt->bind_param("s", $category);
         return $stmt->execute();
     }
 
-    public function insertProduct($name, $price, $description, $image, $quantity, $category) {
+    public function insertProduct($name, $price, $description, $image, $quantity, $category)
+    {
         $query = "INSERT INTO PRODOTTO (nome, prezzo, descrizione, immagine, disponibilita, App_nome) VALUES (?, ?, ?, ?, ?, ?)";
         $stmt = $this->db->prepare($query);
         $stmt->bind_param("sdssis", $name, $price, $description, $image, $quantity, $category);
@@ -148,7 +175,8 @@ class DatabaseHelper {
         return $stmt->insert_id;
     }
 
-    public function getCategories() {
+    public function getCategories()
+    {
         $query = "SELECT nome as name FROM CATEGORIA_PRODOTTO";
         $stmt = $this->db->prepare($query);
         $stmt->execute();
@@ -156,7 +184,8 @@ class DatabaseHelper {
         return $result->fetch_all(MYSQLI_ASSOC);
     }
 
-    public function getOrders($user) {
+    public function getOrders($user)
+    {
         $stmt = $this->db->prepare('SELECT o.riferimento AS riferimento, o.data AS data, SUM(p.prezzo * r.quantita) AS totale
             FROM ORDINE o JOIN richiesta r ON o.riferimento = r.riferimento JOIN PRODOTTO p ON r.codProdotto = p.codProdotto
             WHERE o.email=?
@@ -168,7 +197,8 @@ class DatabaseHelper {
         return $result->fetch_all(MYSQLI_ASSOC);
     }
 
-    public function getAdminPassword($username) {
+    public function getAdminPassword($username)
+    {
         $query = "SELECT password FROM admin WHERE username = ?";
         $stmt = $this->db->prepare($query);
         $stmt->bind_param('s', $username);
@@ -177,7 +207,8 @@ class DatabaseHelper {
         return $result->fetch_all(MYSQLI_ASSOC);
     }
 
-    public function getUserPassword($email) {
+    public function getUserPassword($email)
+    {
         $query = "SELECT password FROM utente WHERE email = ?";
         $stmt = $this->db->prepare($query);
         $stmt->bind_param('s', $email);
@@ -186,19 +217,22 @@ class DatabaseHelper {
         return $result->fetch_all(MYSQLI_ASSOC);
     }
 
-    public function markNotificationAsRead($notification_id) {
+    public function markNotificationAsRead($notification_id)
+    {
         $stmt = $this->db->prepare("UPDATE notifica SET letto=1 WHERE id_notifica=?");
         $stmt->bind_param("i", $notification_id);
         $stmt->execute();
     }
 
-    public function removeNotification($notification_id) {
+    public function removeNotification($notification_id)
+    {
         $stmt = $this->db->prepare("DELETE FROM notifica WHERE id_notifica=?");
         $stmt->bind_param("i", $notification_id);
         $stmt->execute();
     }
-    
-    public function createOrder($email) {
+
+    public function createOrder($email)
+    {
         $stmt = $this->db->prepare('INSERT INTO ordine (email, data, stato) VALUES (?, CURRENT_DATE, "confermato")');
         $stmt->bind_param("s", $email);
         $stmt->execute();
@@ -206,10 +240,10 @@ class DatabaseHelper {
         $inserted_id = $stmt->insert_id;
 
         $stmt->close();
-        
+
         $stmt2 = $this->db->prepare("INSERT INTO richiesta (riferimento, codProdotto, quantita) VALUES ($inserted_id, ?, ?)");
         $cart = $this->getShoppingCart($email);
-        foreach($cart as $product) {
+        foreach ($cart as $product) {
             $stmt2->bind_param("si", $product["codProdotto"], $product["quantita"]);
             $stmt2->execute();
         }
@@ -221,22 +255,25 @@ class DatabaseHelper {
         return $inserted_id;
     }
 
-    public function getOrderDetails($order_id) {
+    public function getOrderDetails($order_id)
+    {
         $stmt = $this->db->prepare("SELECT prodotto.nome, richiesta.quantita, prodotto.prezzo FROM richiesta, prodotto WHERE richiesta.riferimento=? AND richiesta.codProdotto=prodotto.codProdotto");
         $stmt->bind_param("i", $order_id);
         $stmt->execute();
         $result = $stmt->get_result();
         return $result->fetch_all(MYSQLI_ASSOC);
     }
-    
-    public function getAllOrders() {
+
+    public function getAllOrders()
+    {
         $stmt = $this->db->prepare("SELECT ordine.riferimento, ordine.email, ordine.data, SUM(richiesta.quantita) as totale, ordine.stato FROM ordine, richiesta, prodotto WHERE ordine.riferimento=richiesta.riferimento AND prodotto.codProdotto=richiesta.codProdotto GROUP BY ordine.riferimento ORDER BY ordine.data DESC");
         $stmt->execute();
         $result = $stmt->get_result();
         return $result->fetch_all(MYSQLI_ASSOC);
     }
 
-    public function getSingleOrder($order_id) {
+    public function getSingleOrder($order_id)
+    {
         $stmt = $this->db->prepare("SELECT * FROM ordine WHERE riferimento=?");
         $stmt->bind_param("i", $order_id);
         $stmt->execute();
@@ -244,34 +281,38 @@ class DatabaseHelper {
         return $result->fetch_all(MYSQLI_ASSOC);
     }
 
-    public function deleteOrder($order_id) {
+    public function deleteOrder($order_id)
+    {
         $stmt = $this->db->prepare("DELETE FROM ordine WHERE riferimento=?");
         $stmt->bind_param("i", $order_id);
         $stmt->execute();
     }
 
-    public function updateOrderStatus($order_id, $status) {
+    public function updateOrderStatus($order_id, $status)
+    {
         $stmt = $this->db->prepare("UPDATE ordine SET stato=? WHERE riferimento=?");
         $stmt->bind_param("si", $status, $order_id);
         $stmt->execute();
     }
 
-    public function setOrderShipped($order_id) {
+    public function setOrderShipped($order_id)
+    {
         $stmt = $this->db->prepare("UPDATE ordine SET spedito=1 WHERE riferimento=?");
         $stmt->bind_param("i", $order_id);
         $stmt->execute();
     }
 
-    public function createUserNotification($email, $order_id, $title, $description) {
+    public function createUserNotification($email, $order_id, $title, $description)
+    {
         $stmt = $this->db->prepare("INSERT INTO notifica (Data, Titolo, Descrizione, riferimento, email) VALUES (CURRENT_DATE, ?, ?, ?, ?)");
         $stmt->bind_param("ssis", $title, $description, $order_id, $email);
         $stmt->execute();
     }
 
-    public function createAdminNotification($user, $order_id, $title, $description) {
+    public function createAdminNotification($user, $order_id, $title, $description)
+    {
         $stmt = $this->db->prepare("INSERT INTO notifica (Data, Titolo, Descrizione, riferimento, username) VALUES (CURRENT_DATE, ?, ?, ?, ?)");
         $stmt->bind_param("ssis", $title, $description, $order_id, $user);
         $stmt->execute();
     }
 }
-?>
