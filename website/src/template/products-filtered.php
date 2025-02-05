250f50fc5b609df16683d4aefadbb67bdb4b6011
Luca Venturini <luca.venturini14@studio.unibo.it>
2025-02-05 10:52:56 +0100

    added max price param

--- website/src/template/products-filtered.php
+++ website/src/template/products-filtered.php
@@ -4,8 +4,9 @@ require "../bootstrap.php";
 $category = isset($_GET["category"]) ? $_GET["category"] : "";;
 $sort = isset($_GET["sort"]) ? $_GET["sort"] : "";
 $name = isset($_GET["name"]) ? $_GET["name"] : "";
+$maxPrice = isset($_GET["maxPrice"]) ? $_GET["maxPrice"] : "";
 
-$products = $dbh->getProducts($category,$name, $sort);
+$products = $dbh->getProducts($category,$name, $sort, $maxPrice);
 
 require "display-products.php";
 display($products);
